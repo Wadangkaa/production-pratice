130607d03cbe78400135cd2d6bd990edcf1c1185
David Chamling Rai <Davidrai441@gmail.com>
2023-11-07 00:19:49 +0545

    tab width changed from 2 to 4

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose"
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(
-      `${process.env.MONGODB_URI}/${process.env.DATABASE_NAME}`
-    )
-    console.log("Connected to MongoDB!")
-  } catch (error) {
-    console.error(`Database error: ${error}`)
-  }
+    try {
+        await mongoose.connect(
+            `${process.env.MONGODB_URI}/${process.env.DATABASE_NAME}`
+        )
+        console.log("Connected to MongoDB!")
+    } catch (error) {
+        console.error(`Database error: ${error}`)
+    }
 }
 
 export default connectDB
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv"
 import connectDatabase from "./db/connection.js"
 
 dotenv.config({
-  path: "./env",
+    path: "./env",
 })
 
 connectDatabase()
