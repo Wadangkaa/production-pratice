d55026a17673bc1f7e5d0318f6375e6f6d081475
David Chamling Rai <Davidrai441@gmail.com>
2023-11-22 01:46:21 +0545

    routes and controller; video: 12

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
-app = express()
+const app = express()
 
 app.use(
     cors({
@@ -14,5 +14,11 @@ app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+// import routes
+import userRoutes from "./routes/userRoutes.js"
+
+// declear routes
+app.use("/user", userRoutes)
 
 export default app
