f88cd28d4591f3516645ef5bd5b370cc42c20656
David Chamling Rai <Davidrai441@gmail.com>
2023-11-21 18:28:35 +0545

    costume api response and error handling: video:9

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv"
 import connectDatabase from "./db/connection.js"
+import app from "./app.js"
 
 dotenv.config({
     path: "./env",
 })
 
 connectDatabase()
+    .then(() => {
+        const port = process.env.port
+        app.listen(port, () => {
+            console.log(`server listenning at: ${port}`)
+        })
+    })
+    .catch((error) => {
+        console.log("database connection failed in index.js file", error)
+    })
